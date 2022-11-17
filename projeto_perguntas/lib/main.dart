e4976f6b590ca7bdffa8761e3d42cd6ec701050d
Murilo Ramos <okamael.adc@gmail.com>
2022-11-17 07:11:48 -0300

    Criação formulario

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,157 +1,27 @@
 import 'package:flutter/material.dart';
-
+import 'screens/tela_inicial.dart';
+import 'screens/form_screen.dart';
 void main() {
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  bool opacidade = true;
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Container(),
-          title: Text('Tarefas'),
-        ),
-        body: ListView(
-          children: [
-            Task('Aprender Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
-            Task('Andar de Bike',
-                'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg'),
-            Task('Meditar',
-                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
-            Task('Ler',
-                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg'),
-            Task('Jogar',
-                'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg')
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
-      ),
-    );
-  }
-}
-
-class Task extends StatefulWidget {
-  final String nome;
-  final String foto;
-
-  const Task(this.nome, this.foto, {super.key});
-
-  @override
-  State<Task> createState() => _TaskState();
-}
-
-class _TaskState extends State<Task> {
-  int nivel = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Stack(
-          children: [
-            Container(
-              height: 140,
-              color: Colors.blue,
-            ),
-            Column(
-              children: [
-                Container(
-                  height: 100,
-                  color: Colors.white,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        color: Colors.black26,
-                        width: 72,
-                        height: 100,
-                        child: Image.network(widget.foto, fit: BoxFit.cover),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                              width: 200,
-                              child: Text(
-                                widget.nome,
-                                style: TextStyle(fontSize: 24),
-                                overflow: TextOverflow.ellipsis,
-                              )),
-                          Row(
-                            children: [
-                              Icon(Icons.star, size: 15, color: Colors.blue),
-                              Icon(Icons.star, size: 15, color: Colors.blue),
-                              Icon(Icons.star, size: 15, color: Colors.blue),
-                              Icon(Icons.star,
-                                  size: 15, color: Colors.blue[100]),
-                              Icon(Icons.star,
-                                  size: 15, color: Colors.blue[100])
-                            ],
-                          ),
-                        ],
-                      ),
-                      Container(
-                        height: 52,
-                        width: 52,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              nivel++;
-                            });
-
-                            print(nivel);
-                          },
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Icon(Icons.arrow_drop_up),
-                              Text(
-                                'UP',
-                                style: TextStyle(fontSize: 12),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Container(
-                          width: 200,
-                          child: LinearProgressIndicator(
-                            color: Colors.white,
-                            value: nivel / 10,
-                          )),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        'Nivel: $nivel',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+      home: FormScreen(),
     );
   }
 }
